b43abe0dc5716775595351d82fd42d7642f5e687
AiNI <aini.aikido@gmail.com>
2018-12-24 18:28:38 +0900

    公文ミスを修正

--- resources/views/layouts/loginbutton.blade.php
+++ resources/views/layouts/loginbutton.blade.php
@@ -1,7 +1,7 @@
 <?php
-if ({{$logon}}){
-    echo '<a href="/">マイページ</a>　<a href="/logoff">ログアウト</a>'
+if ( $data['logon'] ){
+    echo '<a href="/">マイページ</a>　<a href="/logoff">ログアウト</a>';
 }else{
-    echo '<a href="/login/twitter">Twitter連携でログイン</a>'
+    echo '<a href="/login/twitter">Twitter連携でログイン</a>';
 }
 ?>
